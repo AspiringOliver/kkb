b8d2bf39ccc65708f7fb159e91d34c3a33e50e29
AspiringOliver <64181482+AspiringOliver@users.noreply.github.com>
2020-04-24 09:39:19 +0800

    123

--- 1 基础语法/课后作业/17-模块使用_练习题.py
+++ 1 基础语法/课后作业/17-模块使用_练习题.py
@@ -1,43 +1,45 @@
-import csv
+# import csv
+#
+# with open("mytest.csv", 'a', encoding='utf-8-sig')  as r:
+#     writ = csv.writer(r)
+#     writ.writerow([41, 42, 43])
+#     print("写入完毕！")
+# with open("mytest.csv", encoding='utf-8-sig')  as r:
+#     rea = csv.reader(r)
+#     # 使用csv的reader()方法，创建一个reader对象
+#     for content in rea:
+#         # 遍历rea对象的每一行
+#         print(content)
+# print("读取完毕！")
 
-with open("mytest.csv", 'a', encoding='utf-8-sig')  as r:
-    writ = csv.writer(r)
-    writ.writerow([41, 42, 43])
-    print("写入完毕！")
-with open("mytest.csv", encoding='utf-8-sig')  as r:
-    rea = csv.reader(r)
-    # 使用csv的reader()方法，创建一个reader对象
-    for content in rea:
-        # 遍历rea对象的每一行
-        print(content)
-print("读取完毕！")
 
-# import auxiliary, time
-# ticks1 = time.time()
-# localtime1 = time.localtime(time.time())
-# localtime2 = time.asctime(localtime1)
-# print('本地时间（方式1）：', localtime2)
-# print('请开始输入算式:\n（每次结束一次操作请按一次回车）\n：')
-# sig = 'null'
-#
-# while 1:
-#     auxiliary.number()
-#     if auxiliary.flag == 1:
-#         break
-#     auxiliary.signal()
-#     if auxiliary.flag == 1:
-#         break
-#
-# ticks2 = time.time()
-# print('代码花费时间：', ticks2 - ticks1, '秒')
-#
-# print('='*10, '延时一秒钟', '='*10)
-# time.sleep(1)
-# print('='*10, '延时结束', '='*10)
-#
-# localtime1 = time.localtime(time.time())
-# print('本地时间（localtime）：', localtime1)
-# localtime2 = time.asctime(localtime1)
-# print('本地时间（方式1）：', localtime2)
-# localtime3 = time.strftime('%Y-%m-%d %H:%M:%S', localtime1)
-# print('本地时间（方式2）：', localtime3)
+
+import auxiliary, time
+ticks1 = time.time()
+localtime1 = time.localtime(time.time())
+localtime2 = time.asctime(localtime1)
+print('本地时间（方式1）：', localtime2)
+print('请开始输入算式:\n（每次结束一次操作请按一次回车）\n：')
+sig = 'null'
+
+while 1:
+    auxiliary.number()
+    if auxiliary.flag == 1:
+        break
+    auxiliary.signal()
+    if auxiliary.flag == 1:
+        break
+
+ticks2 = time.time()
+print('代码花费时间：', ticks2 - ticks1, '秒')
+
+print('='*10, '延时一秒钟', '='*10)
+time.sleep(1)
+print('='*10, '延时结束', '='*10)
+
+localtime1 = time.localtime(time.time())
+print('本地时间（localtime）：', localtime1)
+localtime2 = time.asctime(localtime1)
+print('本地时间（方式1）：', localtime2)
+localtime3 = time.strftime('%Y-%m-%d %H:%M:%S', localtime1)
+print('本地时间（方式2）：', localtime3)
--- 2 爬虫/课后作业/第1节 了解爬⾍和浏览器的原理习题.py
+++ 2 爬虫/课后作业/第1节 了解爬⾍和浏览器的原理习题.py
@@ -7,7 +7,7 @@ import requests as re
 #     b.write(photo)
 
 
-#加网站头
+#请求头Request Headers
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
 
